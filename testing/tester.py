5bd1af262f415ba3ef4d18b14df5f7e26cd7c5dc
Caporaso Pasquale <caporaso_pasquale@libero.it>
2020-08-10 12:34:50 +0200

    Aggiunto testing

---
+++ testing/tester.py
@@ -0,0 +1,34 @@
+import requests
+import time
+
+MIN_INTERVAL = 1800
+MAX_INTERVAL = 21600
+
+## 0 = foot
+## 1 = driving
+TRANSPORT = 0
+
+ADDRESS = "http://127.0.0.1:5005"
+
+fd = open("results.csv", "a")
+
+TRIES_PER_INTERVAL = 10
+
+for interval in range(MIN_INTERVAL, MAX_INTERVAL, 600):
+	time_sum = 0.0
+	for _ in range(0, TRIES_PER_INTERVAL):
+
+		start = time.time()
+
+		params = { "latitude" : "41.89332", "longitude" : "12.482932", "interval": interval, "trans": TRANSPORT}
+
+		requests.get(ADDRESS, params=params)
+
+		end = time.time()
+
+		time_sum += end - start
+
+	print(time_sum)
+	average = time_sum / float(TRIES_PER_INTERVAL*1.0)
+	print("Testing complete for interval ", interval, ", average response time: ", average)	
+	fd.write(str(interval) + ", " + str(average) + ", " + str(TRIES_PER_INTERVAL) + "\n")
